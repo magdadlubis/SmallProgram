0db1b8380cbca3fa9dfa4cc2ce0650c4ef4e0460
wojciech-wolny <w.wolny@hotmail.com>
2019-11-10 14:04:57 +0100

    New docstring for dispacher class

--- dispacher.py
+++ dispacher.py
@@ -10,7 +10,7 @@ class WrongParameterError(Exception):
 
 class Dispacher:
     def create_user(self, username: str, password: str) -> User:
-        """Check if user exist in database and return True if password is correct."""
+        """Create user to User table"""
         raise NotImplementedError
 
     def login_user(self, username: str, password: str) -> Union[User, None]:
@@ -18,25 +18,25 @@ class Dispacher:
         raise NotImplementedError
 
     def print_all_users(self) -> List[Union[User, None]]:
-        """"""
+        """Print all users which are in database"""
         raise NotImplementedError
 
     def change_password(self, user: User, new_password: str) -> None:
-        """"""
+        """Chenge password of given user to new one"""
         raise NotImplementedError
 
     def delete_user(self, user: User) -> None:
-        """"""
+        """Delete given user"""
         raise NotImplementedError
 
     def list_messages(self, user: User) -> List[Union[Message, None]]:
-        """"""
+        """Return list of all messages in database for specific user"""
         raise NotImplementedError
 
     def send_message(self, adress: User, sender: User, message: str) -> Message:
-        """"""
+        """Create message to adress (User) to sender (User) into database."""
         raise NotImplementedError
 
     def not_available_option(self):
-        """"""
+        """No other available option"""
         raise WrongParameterError("Wrong parameters set up!")
