92d2152336410637006789e91037c0b9312c2622
wojciech-wolny <w.wolny@hotmail.com>
2020-02-02 13:31:00 +0100

    Update gitignore and dispacher schema

--- dispacher.py
+++ dispacher.py
@@ -10,32 +10,32 @@ class WrongParameterError(Exception):
 
 class Dispacher:
     """HINT: USERNAME == EMAIL """
-    def create_user(self, username: str, password: str) -> User:
-        """Create user to User table"""
-        raise NotImplementedError
-
     def login_user(self, username: str, password: str) -> Union[User, None]:
         """Check if user exist in database and return True if password is correct."""
         raise NotImplementedError
 
-    def print_all_users(self) -> List[Union[User, None]]:
+    def create_user(self, username: str, password: str) -> User:
+        """Create user to User table"""
+        raise NotImplementedError
+
+    def all_users_list(self) -> List[Union[User, None]]:
         """Print all users which are in database"""
-        raise NotImplementedError
-
-    def change_password(self, user: User, new_password: str) -> None:
-        """Chenge password of given user to new one"""
-        raise NotImplementedError
-
-    def delete_user(self, user: User) -> None:
-        """Delete given user"""
         raise NotImplementedError
 
     def list_messages_to_user(self, user: User) -> List[Union[Message, None]]:
         """Return list of all messages in database for specific user"""
         raise NotImplementedError
 
+    def change_password(self, user: User, new_password: str) -> None:
+        """Chenge password of given user to new one"""
+        raise NotImplementedError
+
     def send_message(self, adress: User, sender: User, message: str) -> Message:
         """Create message to adress (User) to sender (User) into database."""
+        raise NotImplementedError
+
+    def delete_user(self, user: User) -> None:
+        """Delete given user"""
         raise NotImplementedError
 
     def not_available_option(self):
